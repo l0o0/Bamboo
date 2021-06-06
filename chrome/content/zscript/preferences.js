95f46b79b4d5c64df86e224a617960ccf0380063
l0o0 <linxzh1989@gmail.com>
2021-06-06 23:17:00 +0800

    prefwindwos: Save script and check async status

--- chrome/content/zscript/preferences.js
+++ chrome/content/zscript/preferences.js
@@ -4,26 +4,50 @@
 Components.utils.import('chrome://zscript/content/zscript.js');
 Components.utils.import("resource://gre/modules/Services.jsm");
 
+
 // eslint-disable-next-line no-unused-vars
 // function initializePrefWindow() {
 //     keyconfigOnLoad();
 // }
 
-// eslint-disable-next-line no-unused-vars
+// Load default config and prepare preference windows
+function init() {
+    return;
+}
+
 function saveScript() {
     let name = document.getElementById("script-name");
     let content = document.getElementById("script-content");
+    let disable = document.getElementById("disable");
     if (name.value in Zscript._scripts) {
-        Zscript._scripts[name.value].
+        Zscript._scripts[name.value].content = content.value;
+        Zscript._scripts[name.value].disable = disable.checked;
+        Zscript._scripts[name.value].isAsync = checkAsync(content.value);
+    } else {
+        Zscript._scripts[name.value] = { name: name.value };
+        Zscript._scripts[name.value].content = content.value;
+        Zscript._scripts[name.value].disable = disable.checked;
+        Zscript._scripts[name.value].isAsync = checkAsync(content.value);
     }
-    Zscript._scripts
+    alert("Saved!");
 }
 
-function updateEditor(idx) {
+function updateEditor(label) {
     var name = document.getElementById("script-name");
     var content = document.getElementById("script-content");
-    name.value = Zscript._scripts[parseInt(idx)].name;
-    content.value = Zscript._scripts[parseInt(idx)].content;
+    var disable = document.getElementById("disable")
+    name.value = label;
+    content.value = Zscript._scripts[label].content;
+    disable.checked = Zscript._scripts[label].disable;
+}
+
+
+function checkAsync(code) {
+    // This check code if from Zotero RunJS in Run Javascript
+    if (code.includes("await")) {
+        return true;
+    }
+    return false;
 }
 
 // eslint-disable-next-line no-unused-vars
